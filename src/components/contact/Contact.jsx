8bff5cc722a4ed51459abc16fbbb3b5f72d53867
Dinesh Chhetri <dinez078@gmail.com>
2025-02-23 20:26:51 -0500

    Updated Contact.jsx: Removed form, kept contact options

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,43 +1,16 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import './contact.css';
 import { HiOutlineMail } from 'react-icons/hi';
 import { TbBrandLinkedin } from 'react-icons/tb';
 import { FaGithub } from 'react-icons/fa';
-import emailjs from 'emailjs-com';
 
 const Contact = () => {
-    const form = useRef();
-    const [isSent, setIsSent] = useState(false);
-    const [error, setError] = useState(null);
-
-    const sendEmail = (e) => {
-        e.preventDefault();
-        setIsSent(false);
-        setError(null);
-
-        emailjs.sendForm(
-            'service_s40ojcs',
-            'template_eh9sppo',
-            form.current,
-            'Kg2rhkR8J5TfgshRJ'
-        )
-            .then((result) => {
-                console.log(result.text);
-                setIsSent(true);
-                form.current.reset();
-            })
-            .catch((error) => {
-                console.error(error.text);
-                setError('Failed to send message. Please try again.');
-            });
-    };
-
     return (
         <section id='contact'>
             <h5>Let's Connect</h5>
             <h2>Contact Me</h2>
 
-           <div className="container contact__container">
+            <div className="container contact__container">
                 <div className="contact__options">
                     <article className="contact__option">
                         <HiOutlineMail className='contact__option-icon' />
@@ -56,13 +29,10 @@ const Contact = () => {
                     <article className="contact__option">
                         <FaGithub className='contact__option-icon' />
                         <h4>GitHub</h4>
-                        <h5>github.com/dinesh</h5>
+                        <h5>github.com/dinesh500224471</h5>
                         <a href="https://github.com/dinesh500224471" target="_blank" rel="noopener noreferrer">Visit My GitHub</a>
                     </article>
                 </div>
-                {/* End of contact options */}
-
-                
             </div>
         </section>
     );
